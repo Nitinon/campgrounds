3fae01f6ef95c51ae23fadb73b90b8ceec147bd4
Nitinon <Nitinon556@hotmail.com>
2018-10-14 13:38:52 +0700

    Edit comment โดยใครก็ได้

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,27 @@ router.post("/",function(req,res){
         }
     });
 });
+
+router.get("/:comment_id/edit",function(req,res){
+    Comment.findById(req.params.comment_id,function(err,foundComment){
+        if(err){
+            res.redirect("back");
+        }else{
+            // โยน camp id กับ comment ที่เจอเข้าไป
+            res.render("comments/edit",{campground_id : req.params.id,comment:foundComment});
+        }
+    });
+});
+router.put("/:comment_id",function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updatedCommet){
+        if(err){
+            res.redirect("back");
+        }else{
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    });
+});
+
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
         return next();
